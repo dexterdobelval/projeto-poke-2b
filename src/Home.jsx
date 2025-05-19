6dfcfc46e256a135d3fdbbc8d726127e8e8d4c09
vt <vitorhugobate14@gmail.com>
2025-05-19 12:08:39 +0000

    flex

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,14 +1,22 @@
 import poke from "./assets/pokeapi_256.png"
+import eevee from './assets/eevee.jpg'
+import './home.css'
 
 const Home = ()=> {
     return (
         <div className="pokeflex">
             <img src={poke} />
+            <img src={eevee} />
             <img src={poke} />
+            <img src={eevee} />
             <img src={poke} />
+            <img src={eevee} />
             <img src={poke} />
+            <img src={eevee} />
             <img src={poke} />
+            <img src={eevee} />
             <img src={poke} />
+            <img src={eevee} />
         </div>
     )
 }
